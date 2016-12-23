04b9645bae67de3304001b9c99851be40d8cee86
Algahtani <jcodeprof@gmail.com>
2016-12-24 01:00:11 +0400

    add scopre variables

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,13 @@ $(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
   });
+
+  $("div#click-one").click(function(event) {
+    var whatToSay = "Hello!";
+    alert(whatToSay);
+  });
+
+  $("div#click-two").click(function(event) {
+    alert(whatToSay);
+  });
 });
